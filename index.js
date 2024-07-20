2b62ed19cb12b84a5076867f2fa2cd08da438d6c
Maingron <r@maingron.com>
2024-07-20 17:33:19 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,76 @@
+function seededRandom(seed = 0) {
+	let rollCount = 0;
+	let previousResult = 1;
+
+	const config = {
+		internalMax: Number.MAX_SAFE_INTEGER,
+		internalNoise: 98470239817094
+
+	}
+
+	let getNewBaseSeed = function(seed = seed, rollCount = rollCount, previousResult = previousResult) {
+		let result = (seed + 1) * (rollCount + 1) * (previousResult + 2);
+		result = (result * config.internalNoise) % config.internalMax;
+
+		return result;
+	}
+
+	let rollInternal = function(min, max, rollCount, previousResult) {
+		let result = 0;
+
+		max = max + 0.1;
+
+		result = getNewBaseSeed(seed, rollCount, previousResult);
+
+		result = result % max;
+
+		result = Math.round(result);
+
+		return result;
+	}
+
+	this.blackbox = {
+		roll: function(min, max) {
+			rollCount++;
+	
+			let result = rollInternal(min, max, rollCount, previousResult);
+	
+			previousResult = result;
+	
+			return result;
+		},
+
+		getArray: function(min, max, arrayLength) {
+			var result = [];
+			for(var i = 0; i < arrayLength; i++) {
+				result.push(this.roll(min, max));
+			}
+	
+			return result;
+		}
+	}
+
+	this.custom = {
+		roll: function(min, max, rollCount, previousResult) {
+			rollCount++;
+	
+			let result = rollInternal(min, max, rollCount, previousResult);
+	
+			return result;
+		},
+
+		getArray: function(min, max, arrayLength, rollCount, previousResult) {
+			var result = [];
+
+			for(var i = 0; i < arrayLength; i++) {
+				let resultThisRoll = this.roll(min, max, rollCount, previousResult);
+				result.push(resultThisRoll);
+				rollCount++;
+				previousResult = resultThisRoll;
+			}
+
+			return result;
+
+		}
+	}
+}
